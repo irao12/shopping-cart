2df2bb17049dd2e8e65e3e503d4c3b3c662b3b0c
Ivan Rao <ivan@Ivans-Mac-Mini.local>
2022-10-25 09:36:08 -0400

    added an input field for quantity and buttons to increment and decrement the quantity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router";
+import { Routes, Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Catalog from "./components/pages/Catalog";
+import products from "./products";
+
 import "./App.css";
 
 function App() {
 	return (
-		<>
-			<Outlet />
-		</>
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/">
+					<Route index element={<Home />}></Route>
+					<Route
+						path="/catalog"
+						element={<Catalog products={products} />}
+					></Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -9,6 +9,15 @@ const Product = ({ name, img, price }) => {
 				<h2>{name}</h2>
 				<h3>${price}</h3>
 			</div>
+			<div className="quantity-selector">
+				<button type="button" className="decrement quantity-button">
+					-
+				</button>
+				<input type="number" max={10}></input>
+				<button type="button" className="increment quantity-button">
+					+
+				</button>
+			</div>
 			<button className="add-to-cart-button">ADD TO CART</button>
 		</div>
 	);
